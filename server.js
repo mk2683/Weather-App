dee9b060dc4256ecc664e12d2d130a339b17a581
mohit kumar <mksujanian2683@gmail.com>
2018-04-02 13:53:25 +0530

    added index.ejs

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require ("express");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.get("/", function(req, res){
-	res.send("Hello World");
+	res.render("index");
+})
+
+app.post("/", function(req, res){
+	res.render("index");
+	console.log(req.body.city);
 })
 
 app.listen(3000, function(){
